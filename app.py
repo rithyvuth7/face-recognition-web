b0ab90bb0070fa22eb7a161da9c0f7857ec007e8
Tha Rithyvuth <tharithyvuth7@gmail.com>
2023-11-23 20:48:12 +0700

    update some frontend

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ from sklearn.metrics import accuracy_score
 app = Flask(__name__)
 
 # Load and preprocess the LFW dataset
-lfw_people = fetch_lfw_people(data_home='/data/lfw_funneled', min_faces_per_person=60)
+lfw_people = fetch_lfw_people(data_home='/data/lfw_funneled', min_faces_per_person=20)
 X = lfw_people.data
 y = lfw_people.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
 
 # Build the SVM model with PCA for dimensionality reduction
 pca = PCA(n_components=150, whiten=True, random_state=42)
-svc = SVC(kernel='rbf', class_weight='balanced')
+svc = SVC(kernel='rbf', class_weight='balanced', probability=True)
 model = make_pipeline(pca, svc)
 model.fit(X_train, y_train)
 
@@ -39,7 +39,12 @@ def predict():
     # Use your model for prediction
     prediction = model.predict(img_array)
 
-    return jsonify({'prediction': str(lfw_people.target_names[prediction[0]])})
+    # Get the probability estimates for each class
+    probabilities = model.predict_proba(img_array)
+    max_probability = np.max(probabilities)  # Get the maximum probability
+    percentage = round(max_probability * 100, 2)  # Calculate the percentage, rounded to 2 decimal places
+    
+    return render_template('index.html', prediction=str(lfw_people.target_names[prediction[0]]), percentage=str(percentage) + '%')
 
 if __name__ == '__main__':
     app.run(debug=True)
