218e9c2455f341b11b5aa07cda1f5deb7f8ed70f
Tha Rithyvuth <tharithyvuth7@gmail.com>
2023-11-26 10:01:19 +0700

    cleanup

--- app.py
+++ app.py
@@ -1,50 +1,77 @@
-from flask import Flask, render_template, request, jsonify
+import cv2
+import streamlit as st
+from joblib import load
 import numpy as np
+import pandas as pd
 from PIL import Image
-import io
-from sklearn.datasets import fetch_lfw_people
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from sklearn.decomposition import PCA
-from sklearn.pipeline import make_pipeline
-from sklearn.metrics import accuracy_score
 
-app = Flask(__name__)
+model = load('model.joblib')
+people_names = pd.read_csv('people_names.csv')
 
-# Load and preprocess the LFW dataset
-lfw_people = fetch_lfw_people(data_home='/data/lfw_funneled', min_faces_per_person=20)
-X = lfw_people.data
-y = lfw_people.target
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
 
-# Build the SVM model with PCA for dimensionality reduction
-pca = PCA(n_components=150, whiten=True, random_state=42)
-svc = SVC(kernel='rbf', class_weight='balanced', probability=True)
-model = make_pipeline(pca, svc)
-model.fit(X_train, y_train)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    file = request.files['file']
-    img = Image.open(io.BytesIO(file.read())).convert('L')  # Convert to grayscale
-    img = img.resize((62, 47))  # Resize the image to match the LFW dataset dimensions
 
-    # Convert the image to a numpy array
-    img_array = np.array(img).reshape(1, -1)
 
-    # Use your model for prediction
-    prediction = model.predict(img_array)
 
-    # Get the probability estimates for each class
-    probabilities = model.predict_proba(img_array)
-    max_probability = np.max(probabilities)  # Get the maximum probability
-    percentage = round(max_probability * 100, 2)  # Calculate the percentage, rounded to 2 decimal places
+def predict(image):
+    img = image
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    faces = face_classifier.detectMultiScale(
+        img,     
+        scaleFactor=1.2,
+        minNeighbors=5,     
+        minSize=(20, 20)
+    )
+
+    for x,y,w,h in faces:
+        img = img[y:y+h,x:x+w]
+        
+    if img.shape[1] > 0 and img.shape[0] > 0:
+        img = cv2.resize(img, (60,60), interpolation = cv2.INTER_AREA)
+
+        hist, bins = np.histogram(img.flatten(), 256, [0, 256])
+
+        cdf = hist.cumsum()
+        cdf_normalized = cdf * hist.max() / cdf.max(),
+        
+        cdf_m = np.ma.masked_equal(cdf, 0)
+        cdf_m = (cdf_m - cdf_m.min())*255 / (cdf_m.max()-cdf_m.min())
+        cdf = np.ma.filled(cdf_m, 0).astype('uint8')
+
+        img = cdf[img]
+        img = img.reshape(60*60)
+
+    scaler_img = scaler.transform(img)
+    pc_img = pca.transform(scaler_img)
+    prediction = model.predict(pc_img)
+
+    #get the probability of the prediction
+    prob = model.predict_proba(pc_img)
+    prob = np.max(prob)
+    return prediction, prob
     
-    return render_template('index.html', prediction=str(lfw_people.target_names[prediction[0]]), percentage=str(percentage) + '%')
+def main():
+    st.title("Face Recognition Web App")
+    st.sidebar.title("Options")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+
+        # Convert the PIL image to a NumPy array
+        image_np = np.array(image)
+
+        # Perform face recognition
+        face_locations, face_encodings = predict(image_np)
+
+        # Display the results
+        for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+            cv2.rectangle(image_np, (left, top), (right, bottom), (0, 255, 0), 2)
+        
+        st.image(image_np, caption="Faces Recognized", use_column_width=True)
+
+if __name__ == "__main__":
+    main()
